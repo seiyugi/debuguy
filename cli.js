21a2a2c83f71e92fbfa8b43d2333d69f62f36635
Yi-Fan Liao <begeeben@gmail.com>
2014-08-26 15:12:40 +0800

    create web server and websocket server in reporter mode to provide graphical information #18
    * Change 'debuguy reporter' to 'debuguy profile'
    * Add web & websocket server
    * Start server on 'debuguy profile'
    * Add public index.html

--- cli.js
+++ cli.js
@@ -3,34 +3,43 @@
 'use strict';
 
 var debuguy = require('./lib/debuguy');
+var server = require('./lib/server');
 
 var userArgs = process.argv;
 var dir = userArgs[3];
 
-if (userArgs.indexOf('-h') !== -1 || userArgs.indexOf('--help') !== -1 || dir === undefined) {
-    return console.log('cli help');
+if (userArgs.indexOf('-h') !== -1 || userArgs.indexOf('--help') !== -1) {
+    return console.log('debuguy help');
 }
 
 if (userArgs.indexOf('-v') !== -1 || userArgs.indexOf('--version') !== -1) {
     return console.log(require('./package').version);
 }
 
-if (userArgs[2] === 'reporter') {
+if (userArgs[2] === 'profile') {
 
   var reporter = new (require('./lib/reporter.js').Reporter)();
   process.stdin.resume();
   process.stdin.setEncoding('utf8');
   var buffer = '';
+  var lines = [];
   reporter.start();
   process.stdin.on('data', function(chunk) {
     buffer = reporter.analyzeBuffer(buffer, chunk);
+    lines.push(buffer);
   });
 
   process.stdin.on('end', function() {
     reporter.analyzeBuffer(buffer, '\n');
+    lines.push(buffer);
     reporter.stop();
+    server.broadcast(lines.join('\n'));
   });
-} else if (userArgs[2] === 'parse') {
+
+  // web and websocket server
+  server.start();
+
+} else if (userArgs[2] === 'parse' && dir !== undefined) {
   var out = userArgs[4];
   debuguy.parse(dir, out);
 }
