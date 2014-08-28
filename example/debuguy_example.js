d13331286ffa3cad23ab96c5f80162d9f27e69f8
mpizza <mmm198219@gmail.com>
2014-08-28 14:33:37 +0800

    add autolog

--- example/debuguy_example.js
+++ example/debuguy_example.js
@@ -1,7 +1,19 @@
 'use strict';
-
 function runDebuguy() {
-  /* debuguy: tag('A') */
+/* debuguy: tag('A') */
+var abc;
+}
+
+var testFn = {
+	init: function _init() {
+
+	},
+
+	stop: function _stop() {
+
+	}
 }
 
 runDebuguy();
+testFn.init();
+testFn.stop();
