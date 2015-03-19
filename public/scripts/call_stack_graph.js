e766a6193993f688a8544fc08af4d12533907a7a
Sean Lee <seanlee@Seans-MacBook-Pro.local>
2015-03-19 16:13:30 +0800

    Add time-diff feature.
    Conflicts:
    	lib/parser.js

--- public/scripts/call_stack_graph.js
+++ public/scripts/call_stack_graph.js
@@ -21,11 +21,14 @@ CallStackGraph.prototype.insertNode = function CallStackGraph_insertNode (data) 
 
     var newNode = document.createElement('div');
     newNode.style.backgroundColor = 'rgb(0,' + ',0)';
+    newNode.id = ('ID' + node.time + node.path[0] + node.path[1]).replace('@', '');
+    console.log(newNode.id);
     var newText = document.createElement('div');
     var funcNameSet = node.path[1].split('@');
     newText.innerHTML = '<span>' + node.path[0] + '</span>' +
       '.<span class="funcName">' + funcNameSet[0] + '</span>' +
-      '<span>@' + funcNameSet[1] + '</span>';
+      '<span>@' + funcNameSet[1] + '</span>' +
+      '<span class="ts"></span>';
     newText.classList.add('nodeDesc');
     var newStack = document.createElement('div');
     newStack.classList.add('stack');
@@ -39,6 +42,10 @@ CallStackGraph.prototype.insertNode = function CallStackGraph_insertNode (data) 
       popNode.path[0] === node.path[0] &&
       popNode.path[1] === node.path[1]) {
       this.currentNode = this.currentNode.parentNode.parentNode;
+      var nodeId = ('ID' + node.time + node.path[0] + node.path[1]).replace('@', '');
+      var timeDiff = node.timeStamp - popNode.timeStamp;
+      var startNode = document.querySelector('#' + nodeId + ' > .nodeDesc > .ts');
+      startNode.textContent = timeDiff;
     }else {
       alert('ERROR: path/time misfit!');
     }
