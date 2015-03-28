7f449c6001e473ec5bf0401dd133cf0dbc4048bc
Yi-Fan Liao <begeeben@gmail.com>
2015-03-28 16:42:05 +0800

    Fix #70 Function names of function members in objects are logged as anonymous functions
    * Add function name support for various kinds of function definitions.
    * Workaround the will be deprecated esprima try/catch handlers array.
    * Fix parsing error for folder names end with '.js'.

--- example/autolog_example.js
+++ example/autolog_example.js
@@ -3,4 +3,46 @@
 
 try {
 } catch (e) {
+  setTimeout(function () {
+    throw e;
+  });
 }
+
+function a (param) {
+  return param;
+}
+
+var b = function (param) {
+  return param;
+};
+
+var c;
+
+c = function (param) {
+  return param;
+};
+
+var obj = {
+  prop: 'prop',
+  func: function (param) {
+    return param;
+  }
+};
+
+obj.c = function (param) {
+  return param;
+};
+
+var d = 'd';
+
+obj[d] = function (param) {
+  return param;
+};
+
+obj['e'] = function (param) {
+  return param;
+};
+
+setTimeout(function () {
+
+});
